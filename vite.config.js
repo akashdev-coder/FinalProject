3e11904903090b4f240476bec50fbb4a65414375
Akash <akash.aubeeluck1@gmail.com>
2024-05-17 08:06:52 +0400

    up vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: process.env.PORT || 3000,  // Use the PORT environment variable or default to 3000
+    host: '0.0.0.0',  // Bind to all network interfaces
   },
   preview: {
     port: process.env.PORT || 3000,  // Use the PORT environment variable or default to 3000
+    host: '0.0.0.0',  // Bind to all network interfaces
   },
 });
