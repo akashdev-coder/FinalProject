67b483a483f7db0bbb8c53558d81587c2b895ce4
Akash <akash.aubeeluck1@gmail.com>
2024-05-18 14:05:39 +0400

    new changes

--- src/components/Freebook.jsx
+++ src/components/Freebook.jsx
@@ -60,10 +60,10 @@ function Freebook() {
     ],
   };
   return (
-    <>
+    <> 
       <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
         <div>
-          <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
+          <h1 className="font-semibold text-xl pb-2">Free Offered Courses AKash</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             Accusantium veritatis alias pariatur ad dolor repudiandae eligendi
